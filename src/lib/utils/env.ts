1e96d9bdbd4c36ba90124c965652dbee63044c66
hawchien.chung <hawchien.chong@atoz-software.tech>
2026-01-13 15:51:48 +0800

    add cn funtion, auth lib and type safe env

--- src/lib/utils/env.ts
+++ src/lib/utils/env.ts
@@ -1,11 +1,15 @@
-import z from "zod";
+import { z } from "zod";
 
 export const env = z
 	.object({
+		BETTER_AUTH_SECRET: z.string().min(1),
+		BETTER_AUTH_URL: z.string().url(),
 		GOOGLE_CLIENT_ID: z.string().min(1),
 		GOOGLE_CLIENT_SECRET: z.string().min(1),
 	})
 	.parse({
+		BETTER_AUTH_SECRET: process.env.BETTER_AUTH_SECRET,
+		BETTER_AUTH_URL: process.env.BETTER_AUTH_URL,
 		GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
 		GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
 	});
