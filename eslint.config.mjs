9d3e426fab506377514e81e7c8059c16e2e37453
hawchien.chung <hawchien.chong@atoz-software.tech>
2026-01-13 17:46:31 +0800

    add eslint and prettier configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig, globalIgnores } from 'eslint/config'
 import { tanstackConfig } from '@tanstack/eslint-config'
 import convexPlugin from '@convex-dev/eslint-plugin'
+import prettierConfig from 'eslint-config-prettier'
 
 export default defineConfig([
   ...tanstackConfig,
   ...convexPlugin.configs.recommended,
-  globalIgnores(['convex/_generated']),
+  prettierConfig,
+  globalIgnores(['convex/_generated', 'dist', '.output', 'src/routeTree.gen.ts']),
 ])
---
+++ prettier.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import("prettier").Config} */
+export default {
+	endOfLine: "lf",
+	printWidth: 100,
+	tabWidth: 4,
+	semi: true,
+	singleQuote: false,
+	trailingComma: "all",
+};
