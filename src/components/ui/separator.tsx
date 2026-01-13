d29766d3293eb430bf57cf8526aab15d3accd5c0
hawchien.chung <hawchien.chong@atoz-software.tech>
2026-01-13 15:50:55 +0800

    add shadcn alert, button, input, label and separator components

---
+++ src/components/ui/separator.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+
+import { cn } from "~/lib/utils/cn";
+
+export type SeparatorProps = React.HTMLAttributes<HTMLDivElement>;
+
+export function Separator({ className, ...props }: SeparatorProps) {
+	return <div className={cn("h-px w-full bg-border", className)} {...props} />;
+}
+
